c7123bb084bc7c3b3a0be862511659192af413c4
tagasouichiro <stitow14@gmail.com>
2017-01-05 23:19:38 +0900

    add multiple handler

--- lib/handler.go
+++ lib/handler.go
@@ -1,7 +1,29 @@
 package lib
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func Handler() {
-	fmt.Println("Hello World!")
+var addr *string
+
+func SetPort(port string) {
+	addr = flag.String("addr", ":"+port, "http service address")
+}
+
+func Handler(urls []string) {
+	fmt.Println(urls)
+	for _, url := range urls {
+		http.Handle(url, http.HandlerFunc(allHandler))
+	}
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func allHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "test")
 }
--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
 	"./lib"
+	"gopkg.in/yaml.v2"
 )
 
+type Resource struct {
+	Url []string
+}
+
 func main() {
-	lib.Handler()
+	r, err := os.Open("./resource.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+	urls := readResource(r)
+
+	lib.SetPort("8080")
+	lib.Handler(urls)
+}
+
+// readResource return resource file
+func readResource(r io.Reader) []string {
+	param, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var resource Resource
+	err = yaml.Unmarshal(param, &resource)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resource.Url
 }
